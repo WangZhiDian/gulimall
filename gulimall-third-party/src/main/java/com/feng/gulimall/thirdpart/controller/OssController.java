bd3419f17a15d96e6a363d2a862901abea5ffb29
wang_zhidian@163.com <wang_zhidian@163.com>
2022-11-11 00:00:13 +0800

    update

--- gulimall-third-party/src/main/java/com/feng/gulimall/thirdpart/controller/OssController.java
+++ gulimall-third-party/src/main/java/com/feng/gulimall/thirdpart/controller/OssController.java
@@ -18,6 +18,8 @@ import java.util.Map;
 @RestController
 public class OssController {
 
+
+
     @RequestMapping("/oss/policy")
     public R policy() {
 
@@ -26,8 +28,8 @@ public class OssController {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-chengdu.aliyuncs.com";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI5tFehPjGdig5dZMdDDMi";
-        String accessKeySecret = "TeMVZJuD5yY5tby1xCiFVsvQvXz9ig";
+        String accessKeyId = "";
+        String accessKeySecret = "";
 
         String bucket = "gulimall-wangzhidian"; // 请填写您的 bucketname 。
         String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
--- gulimall-third-party/src/test/java/com/feng/gulimall/thirdpart/GulimallThirdPartyApplicationTest.java
+++ gulimall-third-party/src/test/java/com/feng/gulimall/thirdpart/GulimallThirdPartyApplicationTest.java
@@ -17,8 +17,8 @@ class GulimallThirdPartyApplicationTest {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-chengdu.aliyuncs.com";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI5tFehPjGdig5dZMdDDMi";
-        String accessKeySecret = "TeMVZJuD5yY5tby1xCiFVsvQvXz9ig";
+        String accessKeyId = "";
+        String accessKeySecret = "";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
